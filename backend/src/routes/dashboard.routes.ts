3b45df5e21f7e9c6d8cc74a0f4e16e0ce3da379c
Divyansh Pal <essencedivine.connect@gmail.com>
2026-07-28 12:58:02 +0530

    Delete backend directory

--- backend/src/routes/dashboard.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { dashboardController } from '../controllers/dashboard.controller.js';
-
-const router = Router();
-
-router.get('/metrics', (req, res, next) => dashboardController.getMetrics(req, res, next));
-router.get('/charts', (req, res, next) => dashboardController.getChartData(req, res, next));
-
-export default router;
